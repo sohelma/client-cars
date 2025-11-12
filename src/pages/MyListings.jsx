a77c6ab983682832fb1f1e4a020a3c595c9088f5
Sohel Mamun <GitHub@example.com>
2025-11-12 22:50:36 +0600

    update my MyListing page

--- src/pages/MyListings.jsx
+++ src/pages/MyListings.jsx
@@ -147,7 +147,7 @@ const MyListings = ({ userEmail }) => {
         </table>
       )}
 
-      {/* 🟢 Update Modal */}
+      {/* Update Modal */}
       {editingCar && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white p-6 rounded-lg w-full max-w-md">
